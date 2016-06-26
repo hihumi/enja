1c42bebbdf706098381e1aa0260a91b39eb0ed51
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-06-26 20:32:00 +0900

    add file

---
+++ enja/outlook.go
@@ -0,0 +1,32 @@
+package enja
+
+import (
+	"fmt"
+)
+
+type OutlookStruct struct {
+	outlookEnString, outlookJaString, outlook string
+}
+
+func OutlookEnString() string {
+	var OutlookEn OutlookStruct
+	OutlookEn.outlookEnString = "outlook"
+
+	return OutlookEn.outlookEnString
+}
+
+func OutlookJaString() string {
+	var OutlookJa OutlookStruct
+	OutlookJa.outlookJaString = `[名]: [C]: [主な意味]: 1. 見通し 2. 視野 3. 展望`
+
+	return OutlookJa.outlookJaString
+}
+
+func OutlookPrintJa() {
+	var Outlook OutlookStruct
+	Outlook.outlook = OutlookJaString()
+
+	fmt.Println(Outlook.outlook)
+
+	return
+}
