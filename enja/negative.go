1c2db6769a6278110cd8bc7f4b4e7c3ab2e4f781
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-06-26 15:31:57 +0900

    add file

---
+++ enja/negative.go
@@ -0,0 +1,33 @@
+package enja
+
+import (
+	"fmt"
+)
+
+type NegativeStruct struct {
+	negativeEnString, negativeJaString, negative string
+}
+
+func NegativeEnString() string {
+	var NegativeEn NegativeStruct
+	NegativeEn.negativeEnString = "negative"
+
+	return NegativeEn.negativeEnString
+}
+
+func NegativeJaString() string {
+	var NegativeJa NegativeStruct
+	NegativeJa.negativeJaString = `[形]: [主な意味]: 1. 悲観的な、消極的な 2. 否定の 3. 負(マイナス)の性質
+(また、写真の「ネガ」: a negative film)`
+
+	return NegativeJa.negativeJaString
+}
+
+func NegativePrintJa() {
+	var Negative NegativeStruct
+	Negative.negative = NegativeJaString()
+
+	fmt.Println(Negative.negative)
+
+	return
+}
