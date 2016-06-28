149ddc76417cea848f74a1978c5f53270ebb5a95
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-06-28 11:14:15 +0900

    add file

---
+++ enja/maintain.go
@@ -0,0 +1,32 @@
+package enja
+
+import (
+	"fmt"
+)
+
+type MaintainStruct struct {
+	maintainEnString, maintainJaString, maintain string
+}
+
+func MaintainEnString() string {
+	var MaintainEn MaintainStruct
+	MaintainEn.maintainEnString = "maintain"
+
+	return MaintainEn.maintainEnString
+}
+
+func MaintainJaString() string {
+	var MaintainJa MaintainStruct
+	MaintainJa.maintainJaString = `[動]: [主な意味]: 1. (状態や関係など)を維持する、保つ 2. (建物や機械など)を手入れする、整備する 3. 主張する`
+
+	return MaintainJa.maintainJaString
+}
+
+func MaintainPrintJa() {
+	var Maintain MaintainStruct
+	Maintain.maintain = MaintainJaString()
+
+	fmt.Println(Maintain.maintain)
+
+	return
+}
