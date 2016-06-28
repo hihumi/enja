f9d9438db837824ebb079be8de99c618ea3f3c93
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-06-28 10:54:44 +0900

    add file

---
+++ enja/word_lists.go
@@ -0,0 +1,90 @@
+package enja
+
+import (
+	"fmt"
+)
+
+type WordListsStruct struct {
+	wordListsEnString, wordListsJaString, wordLists string
+}
+
+func WordListsEnString() string {
+	var WordListsEn WordListsStruct
+	WordListsEn.wordListsEnString = "word-lists"
+
+	return WordListsEn.wordListsEnString
+}
+
+func WordListsJaString() string {
+	var WordListsJa WordListsStruct
+	WordListsJa.wordListsJaString = `word lists:
+[a]:
+
+[b]:
+
+[c]:
+
+[d]:
+
+[e]:
+
+[f]:
+
+[g]:
+
+[h]:
+
+[i]:
+  individual,
+
+[j]:
+
+[k]:
+
+[l]:
+
+[m]:
+  my,
+
+[n]:
+  negative,
+
+[o]:
+  outlook,
+
+[p]:
+  positive,
+
+[q]:
+
+[r]:
+  respect,
+
+[s]:
+
+[t]:
+
+[u]:
+
+[v]:
+
+[w]:
+  will,
+
+[x]:
+
+[y]:
+
+[z]:`
+
+	return WordListsJa.wordListsJaString
+}
+
+func WordListsPrintJa() {
+	var WordLists WordListsStruct
+	WordLists.wordLists = WordListsJaString()
+
+	fmt.Println(WordLists.wordLists)
+
+	return
+}
