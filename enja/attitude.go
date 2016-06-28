cf553bd34457dc98782c2d7e6f7d3f33f19050bf
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-06-28 15:41:47 +0900

    add file

---
+++ enja/attitude.go
@@ -0,0 +1,32 @@
+package enja
+
+import (
+	"fmt"
+)
+
+type AttitudeStruct struct {
+	attitudeEnString, attitudeJaString, attitude string
+}
+
+func AttitudeEnString() string {
+	var AttitudeEn AttitudeStruct
+	AttitudeEn.attitudeEnString = "attitude"
+
+	return AttitudeEn.attitudeEnString
+}
+
+func AttitudeJaString() string {
+	var AttitudeJa AttitudeStruct
+	AttitudeJa.attitudeJaString = `[名]: [C, U]: [主な意味]: 態度、姿勢、考え方`
+
+	return AttitudeJa.attitudeJaString
+}
+
+func AttitudePrintJa() {
+	var Attitude AttitudeStruct
+	Attitude.attitude = AttitudeJaString()
+
+	fmt.Println(Attitude.attitude)
+
+	return
+}
