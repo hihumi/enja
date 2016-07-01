6fd10cac4c7acab3cb4b98803daa98e03d624a8a
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-07-02 00:17:28 +0900

    add file

---
+++ enja/origin.go
@@ -0,0 +1,32 @@
+package enja
+
+import (
+	"fmt"
+)
+
+type OriginStruct struct {
+	originEnString, originJaString, origin string
+}
+
+func OriginEnString() string {
+	var OriginEn OriginStruct
+	OriginEn.originEnString = "origin"
+
+	return OriginEn.originEnString
+}
+
+func OriginJaString() string {
+	var OriginJa OriginStruct
+	OriginJa.originJaString = `[名]: [C, U]: 起源、発祥、生まれ`
+
+	return OriginJa.originJaString
+}
+
+func OriginPrintJa() {
+	var Origin OriginStruct
+	Origin.origin = OriginJaString()
+
+	fmt.Println(Origin.origin)
+
+	return
+}
