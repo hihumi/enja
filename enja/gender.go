ad867231da139a872ef3a7895b29a4a9b735e7df
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-07-02 01:36:37 +0900

    add file

---
+++ enja/gender.go
@@ -0,0 +1,33 @@
+package enja
+
+import (
+	"fmt"
+)
+
+type GenderStruct struct {
+	genderEnString, genderJaString, gender string
+}
+
+func GenderEnString() string {
+	var GenderEn GenderStruct
+	GenderEn.genderEnString = "gender"
+
+	return GenderEn.genderEnString
+}
+
+func GenderJaString() string {
+	var GenderJa GenderStruct
+	GenderJa.genderJaString = `[名]: [C, U]: [主な意味]: (社会的・文化的に形成される)性別
+(なお、sexは、生物的・肉体的な性別)`
+
+	return GenderJa.genderJaString
+}
+
+func GenderPrintJa() {
+	var Gender GenderStruct
+	Gender.gender = GenderJaString()
+
+	fmt.Println(Gender.gender)
+
+	return
+}
