cff1f27eed64c2865aa654cfd63f14255119ca61
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-07-05 17:37:02 +0900

    update

--- main.go
+++ main.go
@@ -11,7 +11,8 @@ func main() {
 
 	fmt.Println("英単語の簡素な意味を調べることができます。英単語を小文字で入力してください。")
 	fmt.Println("登録されている全英単語を表示したい場合は、words-list と入力してください。")
-	fmt.Println("終了する場合は、q、またはQキーを入力してください。")
+	fmt.Printf("終了する場合は、q、またはQキーを入力してください。\n\n")
+
 	fmt.Printf(">>> ")
 
 	scanner := bufio.NewScanner(os.Stdin)
