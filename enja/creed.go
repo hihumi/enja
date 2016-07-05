7d9c0b5945300dfdd237bcc2b7a8c59ad9423b61
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-07-05 18:55:21 +0900

    add file

---
+++ enja/creed.go
@@ -0,0 +1,32 @@
+package enja
+
+import (
+	"fmt"
+)
+
+type CreedStruct struct {
+	creedEnString, creedJaString, creed string
+}
+
+func CreedEnString() string {
+	var CreedEn CreedStruct
+	CreedEn.creedEnString = "creed"
+
+	return CreedEn.creedEnString
+}
+
+func CreedJaString() string {
+	var CreedJa CreedStruct
+	CreedJa.creedJaString = `[名]: [C]: [主な意味]: 信条、信仰`
+
+	return CreedJa.creedJaString
+}
+
+func CreedPrintJa() {
+	var Creed CreedStruct
+	Creed.creed = CreedJaString()
+
+	fmt.Println(Creed.creed)
+
+	return
+}
