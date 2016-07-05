5902d4f0b4ee8a732489f52aeae7d9698b40c633
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-07-05 15:17:47 +0900

    update

--- main.go
+++ main.go
@@ -1,25 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Printf("--- Welcome! --- \n\n")
 	fmt.Printf("英単語の簡素な意味を調べることができます。英単語を小文字で入力してください。終了するには、q、またはQキーを入力してください。\n")
+	fmt.Printf(">>> ")
 
-	for {
-		fmt.Printf(">>> ")
-		var enWord string
-		fmt.Scanln(&enWord)
+	scanner := bufio.NewScanner(os.Stdin)
 
+	for scanner.Scan() {
 		q := "q"
 		Q := "Q"
-		if q == enWord || Q == enWord {
+		if q == scanner.Text() || Q == scanner.Text() {
 			fmt.Printf("\n--- See you! --- \n")
 			break
 		}
 
-		SwitchEnWord(enWord)
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "おっと！ おそらく読み込みエラーです:", err)
+		}
+
+		SwitchEnWord(scanner.Text())
+
+		fmt.Printf(">>> ")
 	}
 }
